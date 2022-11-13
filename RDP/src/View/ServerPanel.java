cd3a94981274081be99d7cf4bd41fb8425ac8cfc
Nguyễn Nho Song Hoang <93175001+sghoang24@users.noreply.github.com>
2022-11-13 08:54:30 +0700

    fix GUI (#7)

--- RDP/src/View/ServerPanel.java
+++ RDP/src/View/ServerPanel.java
@@ -11,6 +11,8 @@ import javax.swing.JTextField;
 import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.border.EtchedBorder;
+import java.awt.SystemColor;
 
 public class ServerPanel extends JPanel {
 	private JTextField tfPort;
@@ -23,51 +25,54 @@ public class ServerPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public ServerPanel() {
+		setBackground(SystemColor.textHighlight);
 		setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "Server Listening", TitledBorder.LEADING, TitledBorder.TOP, 
-					new Font("Times New Roman", Font.BOLD, 14), Color.BLACK));
-		panel.setBounds(40, 41, 367, 193);
+		panel.setBackground(SystemColor.textHighlight);
+		panel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, 
+				new Color(255, 255, 255), new Color(160, 160, 160)), "Server Listening", 
+				TitledBorder.LEADING, TitledBorder.TOP, new Font("Times New Roman", Font.BOLD, 14), Color.BLACK));
+		panel.setBounds(40, 41, 367, 200);
 		add(panel);
 		panel.setLayout(null);
 		
 		JLabel lbServerIP = new JLabel("Server IP");
-		lbServerIP.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		lbServerIP.setFont(new Font("Times New Roman", Font.BOLD, 13));
 		lbServerIP.setBounds(32, 34, 64, 14);
 		panel.add(lbServerIP);
 		
 		JLabel lbPort = new JLabel("Port");
-		lbPort.setFont(new Font("Times New Roman", Font.BOLD, 12));
-		lbPort.setBounds(32, 71, 64, 14);
+		lbPort.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		lbPort.setBounds(32, 78, 64, 14);
 		panel.add(lbPort);
 		
 		JLabel lbPassword = new JLabel("Set Password");
-		lbPassword.setFont(new Font("Times New Roman", Font.BOLD, 12));
-		lbPassword.setBounds(32, 106, 80, 14);
+		lbPassword.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		lbPassword.setBounds(32, 117, 80, 14);
 		panel.add(lbPassword);
 		
 		cbxIP = new JComboBox();
 		cbxIP.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		cbxIP.setBounds(149, 30, 155, 22);
+		cbxIP.setBounds(149, 30, 155, 27);
 		panel.add(cbxIP);
 		
 		tfPort = new JTextField();
 		tfPort.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		tfPort.setBounds(149, 66, 155, 25);
+		tfPort.setBounds(149, 72, 155, 27);
 		panel.add(tfPort);
 		tfPort.setColumns(10);
 		
 		tfPassword = new JTextField();
 		tfPassword.setFont(new Font("Times New Roman", Font.PLAIN, 12));
 		tfPassword.setColumns(10);
-		tfPassword.setBounds(149, 101, 155, 25);
+		tfPassword.setBounds(149, 111, 155, 27);
 		panel.add(tfPassword);
 		
 		JLabel lbStatus = new JLabel("New label");
-		lbStatus.setForeground(Color.RED);
+		lbStatus.setForeground(new Color(255, 0, 51));
 		lbStatus.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 12));
-		lbStatus.setBounds(31, 157, 65, 14);
+		lbStatus.setBounds(31, 149, 298, 33);
 		panel.add(lbStatus);
 		
 		lbStartListen = new JLabel("Start Listening");
