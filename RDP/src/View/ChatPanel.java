44f47a5ae885a62b1665bd006ab0ebeb2085bd91
Nguyễn Nho Song Hoang <hoangnguyennho24@gmail.com>
2022-11-13 08:53:10 +0700

    fix GUI

--- RDP/src/View/ChatPanel.java
+++ RDP/src/View/ChatPanel.java
@@ -8,6 +8,8 @@ import javax.swing.JTextArea;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.SystemColor;
 
 public class ChatPanel extends JPanel {
 	private JTextField txtMessage;
@@ -16,30 +18,34 @@ public class ChatPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public ChatPanel() {
+		setBackground(SystemColor.textHighlight);
 		setLayout(null);
 		
 		txtMessage = new JTextField();
-		txtMessage.setBounds(10, 263, 339, 73);
+		txtMessage.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		txtMessage.setBounds(10, 244, 339, 54);
 		add(txtMessage);
 		txtMessage.setColumns(10);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 10, 463, 232);
+		scrollPane.setBounds(10, 10, 439, 214);
 		add(scrollPane);
 		
 		JTextArea txtAreaChat = new JTextArea();
+		txtAreaChat.setEditable(false);
+		txtAreaChat.setFont(new Font("Times New Roman", Font.PLAIN, 13));
 		scrollPane.setViewportView(txtAreaChat);
 		
 		JLabel lblSend = new JLabel("Send");
-		lblSend.setIcon(new ImageIcon("C:\\Users\\TECHCARE\\Documents\\GitHub\\PBL4---RemoteDesktop\\RDP\\Images\\send_icon.png"));
-		lblSend.setFont(new Font("Times New Roman", Font.PLAIN, 14));
-		lblSend.setBounds(391, 263, 82, 29);
+		lblSend.setIcon(new ImageIcon("D:\\DUT - Year 3\\PBL4\\PBL4---RemoteDesktop\\RDP\\Images\\send_icon.png"));
+		lblSend.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		lblSend.setBounds(367, 234, 82, 29);
 		add(lblSend);
 		
 		JLabel lblFile = new JLabel("File");
-		lblFile.setFont(new Font("Times New Roman", Font.PLAIN, 14));
-		lblFile.setIcon(new ImageIcon("C:\\Users\\TECHCARE\\Documents\\GitHub\\PBL4---RemoteDesktop\\RDP\\Images\\file_icon.png"));
-		lblFile.setBounds(391, 307, 82, 29);
+		lblFile.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		lblFile.setIcon(new ImageIcon("D:\\DUT - Year 3\\PBL4\\PBL4---RemoteDesktop\\RDP\\Images\\file_icon.png"));
+		lblFile.setBounds(367, 274, 82, 29);
 		add(lblFile);
 
 	}
