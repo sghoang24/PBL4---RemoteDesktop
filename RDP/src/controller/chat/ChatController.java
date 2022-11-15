006eb72b27bc762b9ad11c4a5e529fa4729fa82f
NgoctraiNguyen <nguyennhantamdan@gmail.com>
2022-11-15 08:14:43 +0700

    Up load transfer file TCP

--- RDP/src/controller/chat/ChatController.java
+++ RDP/src/controller/chat/ChatController.java
@@ -1,5 +1,22 @@
 package controller.chat;
 
-public class ChatController {
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
+import model.common.FileInfo;
+
+public class ChatController {
+	private Socket socket;
+
+	public ChatController(Socket socket) {
+		this.socket = socket;
+	}
+	
+	public void Sendfile(FileInfo Myfile) throws IOException {
+		ObjectOutputStream bos = new  ObjectOutputStream(socket.getOutputStream());
+		bos.writeObject(Myfile);
+	}
+	
+	
 }
